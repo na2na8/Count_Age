70e0acbbd5de7bdb92f32f4012ee9cf5fdc8c9de
Nayeon Kim <na2na8@naver.com>
2017-11-22 17:12:42 +0900

    #clear

--- app/src/main/java/com/example/na2na8/count_age/MainActivity.java
+++ app/src/main/java/com/example/na2na8/count_age/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     Button b2;
     EditText et1;
     EditText et2;
+    public void button1Method(View v){
+        String getEdit1 = et1.getText().toString();
+        int getEdit_1 = Integer.parseInt(getEdit1);
+        int age = 2017-getEdit_1+1;
+        Toast.makeText(MainActivity.this, "당신의 나이는 "+age  +"세 입니다.",Toast.LENGTH_LONG).show();
+    }
+
+    public void button2Method(View v){
+        String getEdit2 = et2.getText().toString();
+        int getEdit_2 = Integer.parseInt(getEdit2);
+        int year = 2018 - getEdit_2;
+        Toast.makeText(MainActivity.this, "당신의 태어난 해는 " + year + "년 입니다.", Toast.LENGTH_LONG).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,14 +35,12 @@ public class MainActivity extends AppCompatActivity {
         b1 = (Button)findViewById(R.id.btCount1);
         et1 = (EditText)findViewById(R.id.etYear);
 
+
         b2 = (Button)findViewById(R.id.btCount2);
         et2 = (EditText)findViewById(R.id.etAge);
 
-        b1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                et1.setText("asdf");
-            }
-        });
+
     }
+
+
 }
